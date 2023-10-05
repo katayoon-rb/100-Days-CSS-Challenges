f65179a6cb01362e05a2b988a69ad09a5f6089ae
katayoon-rb <55879886+katayoon-rb@users.noreply.github.com>
2023-10-05 14:12:05 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const container = document.getElementById('container')
+
+
+function importDays(start, end) {
+    for (let i = start; i < end + 1; i++) {
+        let day = start < 10? `0${start}`: start
+
+        container.innerHTML += `
+            <div class="card">
+                <h2 class="num">${day}</h2>
+                <div class="info">
+                    <h3>Day ${day}</h3>
+                    <a href="Day${day}/index.html">
+                        <button type="button">Go to Site</button>
+                    </a>
+                </div>
+            </div>
+        `
+
+        start++
+    }
+}
+
+importDays(1, 29)  // CHANGE //
