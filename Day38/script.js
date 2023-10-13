e43ada1550139575cba481841a6cafe9fd352923
katayoon-rb <55879886+katayoon-rb@users.noreply.github.com>
2023-10-13 21:34:07 +0330

    Add files via upload

---
+++ Day38/script.js
@@ -0,0 +1,4 @@
+document.querySelector('.frame').addEventListener('click', () => {
+	document.querySelector('.white').classList.toggle('big')
+	document.querySelector('.purple').classList.toggle('normal')
+})
